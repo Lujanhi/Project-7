a98d36c52783e74f6d6a20f7115d48bd74f05ab0
Lujanhi <51241524+Lujanhi@users.noreply.github.com>
2019-08-29 11:20:43 -0400

    create a new github

---
+++ src/App.js
@@ -0,0 +1,21 @@
+//The app.js is the base component - combines react with the index.html
+//Import the React module from the react package
+import React, { Component } from 'react';
+//load our component Router (which is the only item embedded)
+import Router from './Router';
+
+import '../index.css';
+
+//App - base component displayed in the index.js ReactDOM.render()
+export default class App extends Component {
+  render() {
+    return (
+      <div className="container">
+        <Router />
+      </div>
+    );
+  }
+}
+
+//"export default class App" does this; comment it out
+//export default App;
---
+++ src/config.js
@@ -0,0 +1,5 @@
+const apiKey = '05ac33baa4b2a6aaed1f2e689018e960'
+export default apiKey;
+
+
+
